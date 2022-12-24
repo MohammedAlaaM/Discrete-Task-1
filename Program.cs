4fd0faf7e01002b29e0239ea88bc10fd0c79312a
MohammedAlaaM <121355281+MohammedAlaaM@users.noreply.github.com>
2022-12-24 19:57:29 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Linq;
+
+namespace Prime_Numbers
+{
+	public static class Program
+	{
+		public static void Main(string[] args)
+		{
+			Console.WriteLine("Enter A Number");
+			int Number1 = int.Parse(Console.ReadLine());
+			for (int Number2 = 2; Number2 < Number1 / 2; Number2++)
+
+			{
+				if (Number1 % Number2 == 0)
+
+				{
+					Console.WriteLine("This Number Is Not Prime ");
+					return;
+				}
+
+			}
+			Console.WriteLine("This Number Is Prime");
+
+		}
+	}
+}
+
+
